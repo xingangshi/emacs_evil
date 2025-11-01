8a9c205107a8509a281f9714cea4603d5b6dd54b
Panshi <vincentsxg@gmail.com>
2025-11-01 08:18:39 +0800

    Update prelude-orgmode.el

--- prelude_configs/prelude-orgmode.el
+++ prelude_configs/prelude-orgmode.el
@@ -216,6 +216,7 @@
      (holiday-fixed 6 1  "儿童节")
      (holiday-fixed 9 10 "教师节")
      (holiday-fixed 6 4 "六四纪念日")
+     (holiday-fixed 10 31 "万圣节")
      (holiday-float 5 0 2 "母亲节")   ;5月的第二个星期天
      (holiday-float 6 0 3 "父亲节")
      (holiday-float 11 4 4 "感恩节")   ;11 月的第 4 个星期四
